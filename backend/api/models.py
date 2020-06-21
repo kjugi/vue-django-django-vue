2d0fc2f24b48388145d28fba0ad08bd090a55569
Filip <filip.kudla@snow.dog>
2020-06-22 00:11:05 +0200

    Add jsonfield to model and use HyperLinkedModelSerializer for post

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,11 +1,12 @@
 from django.core.validators import validate_comma_separated_integer_list
 from django.db import models
+from jsonfield import JSONField
 
 class Writer(models.Model):
     name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
     bio = models.TextField()
-    socialLinks = models.CharField(max_length=255, validators=[validate_comma_separated_integer_list])
+    socialLinks = JSONField(null=True)
     # image = models.ImageField() - pip install Pillow or FilePathField
 
 class Post(models.Model):
@@ -13,6 +14,6 @@ class Post(models.Model):
     content = models.TextField()
     featureImage = models.CharField(max_length=200) # FilePathField
     writer = models.ForeignKey(Writer, on_delete=models.CASCADE)
-    categories = models.CharField(max_length=255, validators=[validate_comma_separated_integer_list])
+    categories = JSONField(null=True)
     areCommentsEnabled = models.BooleanField(default=True)
     # urlSlug = models. - example of custom url slug
--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Post, Writer
 
-class PostSerializer(serializers.ModelSerializer):
+class PostSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Post
         fields = '__all__'
