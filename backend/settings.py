5fb50ea8df3b1c6df2d861903a7aa41a7e4b8a0e
Filip <filip.kudla@snow.dog>
2020-06-27 20:03:45 +0200

    Add rest pagination for 10 items

--- backend/settings.py
+++ backend/settings.py
@@ -99,7 +99,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         # REST_FRAMEWORK is compatible with oauth2 and axes wall ootb
         'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
 
 OAUTH2_PROVIDER = {
