c9b619bca36ecff1ea7236933f2aea9abf9cf2ff
Filip <filip.kudla@snow.dog>
2020-07-03 23:59:16 +0200

    Add writer fields to post serializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,17 +1,19 @@
 from rest_framework import serializers
 from .models import Post, Writer
 
+class WriterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Writer
+        fields = '__all__'
+
 class PostSerializer(serializers.ModelSerializer):
+    writer = WriterSerializer()
     class Meta:
         model = Post
         fields = ['id', 'title', 'content', 'categories', 'featureImage', 'writer']
 
 class SinglePostSerializer(serializers.HyperlinkedModelSerializer):
+    writer = WriterSerializer()
     class Meta:
         model = Post
-        fields = '__all__'
-
-class WriterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Writer
         fields = '__all__'
