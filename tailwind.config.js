bfe1d57840780fc7ca2fd2f656c763a0a5030fb9
Filip <filip.kudla@snow.dog>
2020-07-05 01:29:10 +0200

    Create custom config to tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  theme: {
+    screens: {
+      xsm: '320px',
+      sm: '480px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1200px',
+      xxl: '1600px'
+    },
+    fontFamily: {
+      display: ['Avenir', 'Helvetica', 'Arial', 'sans-serif'],
+      body: ['Avenir', 'Helvetica', 'Arial', 'sans-serif'],
+    },
+    extend: {}
+  },
+  variants: {},
+  plugins: [],
+}
