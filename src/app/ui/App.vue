8473e200b450e18c3b694e872c6807cdc5411f67
Filip <filip.kudla@snow.dog>
2020-07-05 01:30:28 +0200

    Replace styles in app.vue for tailwind classes + add node-sass and sass-loade

--- src/app/ui/App.vue
+++ src/app/ui/App.vue
@@ -1,33 +1,36 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/blog">Blog</router-link>
+  <div class="m-0 font-display">
+    <div class="p-8">
+      <router-link
+        to="/"
+        class="font-bold text-green-400"
+      >
+        Home
+      </router-link>
+
+      <router-link
+        to="/about"
+        class="font-bold text-green-400"
+      >
+        About
+      </router-link>
+
+      <router-link
+        to="/blog"
+        class="font-bold text-green-400"
+      >
+        Blog
+      </router-link>
     </div>
+
     <router-view/>
   </div>
 </template>
 
 <style src="../assets/tailwind.css"></style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+<style lang="scss">
+a.router-link-exact-active {
+  @apply text-gray-600;
 }
 </style>
