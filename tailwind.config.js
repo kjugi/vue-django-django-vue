76ec5ff48f233cfef2badbc47102e5852900a500
Filip <filip.kudla@snow.dog>
2020-07-06 00:19:46 +0200

    Add max-width custom values in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
       display: ['Avenir', 'Helvetica', 'Arial', 'sans-serif'],
       body: ['Avenir', 'Helvetica', 'Arial', 'sans-serif'],
     },
+    maxWidth: {
+      '1/4': '25%',
+      '1/2': '50%',
+      '3/4': '75%',
+    },
     extend: {}
   },
   variants: {},
