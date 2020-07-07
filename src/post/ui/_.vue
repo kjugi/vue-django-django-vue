566887f68d1486dc3727683cf7e6b8421c1b0356
Filip <filip.kudla@snow.dog>
2020-07-07 23:32:08 +0200

    Update single post ui

--- src/post/ui/_.vue
+++ src/post/ui/_.vue
@@ -1,38 +1,39 @@
 <template>
-  <div
-    v-if="data"
-    class='post'
-  >
-    <h3 class="text-2xl my-2">
+  <div v-if="data">
+    <h3 class="title relative text-2xl p-4 mb-8 border-2 border-black text-black">
       {{ data.title }}
     </h3>
 
-    <p class="post__content">
+    <p class="text-justify max-w-screen-md mx-auto">
       {{ data.content }}
     </p>
 
     <div
       v-if="data.categories"
-      class="post__categories"
+      class="mt-8"
     >
       <div
         v-for="(item, index) in data.categories"
         :key="index"
-        class="post__category"
       >
         {{ data.text }}
       </div>
     </div>
 
-    <div class="author relative border-gray-700 border-2 p-4 mt-4">
-      <h4 class="post__author-name">
-        Name: {{ data.writer.name }}
+    <div class="author relative border-gray-700 border-2 p-4 mt-8">
+      <h4 class="mb-4">
+        <span class="border-b-2 border-black font-bold">
+          Name:
+        </span>
+
+        {{ data.writer.name }}
       </h4>
 
-      <p
-        v-if="data.writer.bio"
-        class="post__author-bio"
-      >
+      <p v-if="data.writer.bio">
+        <span class="border-b-2 border-black font-bold">
+          Bio:
+        </span>
+
         {{ data.writer.bio }}
       </p>
     </div>
@@ -74,5 +75,34 @@ export default {
   @apply text-base;
   @apply bg-white;
   @apply px-2;
+}
+
+.title:before {
+  content: '';
+  @apply absolute;
+  background: #3DDC97;
+  background: linear-gradient(90deg, #3ddc97 0%, #75e6b5 60%, #23be7b 100%);
+  background-size: 400% 100%;
+  width: 90%;
+  height: 90%;
+  top: -10px;
+  left: -5%;
+  z-index: -1;
+  @apply transition-all;
+  @apply duration-300;
+  @apply ease-in-out;
+  animation: backgroundAnimation 15s infinite;
+}
+
+@keyframes backgroundAnimation {
+  0% {
+		background-position: 0% 0%;
+	}
+	50% {
+		background-position: 100% 0%;
+	}
+	100% {
+		background-position: 0% 0%;
+	}
 }
 </style>
