19e6742749f01b1838eaafbaac61e105750ea84f
Filip <filip.kudla@snow.dog>
2020-09-05 00:07:20 +0200

    Update backend urls to new jwt url's

--- backend/urls.py
+++ backend/urls.py
@@ -18,7 +18,10 @@ from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
 from rest_framework import routers
-import oauth2_provider.views as oauth2_views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from api.views import indexWelcome, PostViewSet, WriterViewSet, UserViewSet
 
@@ -26,31 +29,6 @@ router = routers.DefaultRouter()
 router.register('post', PostViewSet)
 router.register('author', WriterViewSet)
 router.register('user', UserViewSet)
-
-# OAuth2 provider endpoints
-oauth2_endpoint_views = [
-    path('authorize/', oauth2_views.AuthorizationView.as_view(), name="authorize"),
-    path('token/', oauth2_views.TokenView.as_view(), name="token"),
-    path('revoke-token/', oauth2_views.RevokeTokenView.as_view(), name="revoke-token")
-]
-
-if settings.DEBUG:
-    # OAuth2 Application Management endpoints
-    # TODO: Fix redirect to login page when not logged
-    oauth2_endpoint_views += [
-        path('applications/', oauth2_views.ApplicationList.as_view(), name="list"),
-        path('applications/register/', oauth2_views.ApplicationRegistration.as_view(), name="register"),
-        path('applications/<pk>/', oauth2_views.ApplicationDetail.as_view(), name="detail"),
-        path('applications/<pk>/delete/', oauth2_views.ApplicationDelete.as_view(), name="delete"),
-        path('applications/<pk>/update/', oauth2_views.ApplicationUpdate.as_view(), name="update")
-    ]
-
-    # OAuth2 Token Management endpoints
-    oauth2_endpoint_views += [
-        path('authorized-tokens/', oauth2_views.AuthorizedTokensListView.as_view(), name="authorized-token-list"),
-        path('authorized-tokens/<pk>/delete/', oauth2_views.AuthorizedTokenDeleteView.as_view(),
-            name="authorized-token-delete")
-    ]
 
 urlpatterns = [
     # /
@@ -62,8 +40,11 @@ urlpatterns = [
     # /admin/
     path(os.getenv('ADMIN_URL'), admin.site.urls),
 
-    # /o/<oauth-pages>
-    path('o/', include((oauth2_endpoint_views, 'o'), namespace='oauth2_provider'))
+    # /token/<jwt-tokens>
+    path('token/', include([
+        path('', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+        path('refresh/', TokenRefreshView.as_view(), name='token_refresh')
+    ]))
 ]
 
 handler403 = 'rest_framework.exceptions.server_error'
