65e3d1738bca748a4c4afa5f0687abc7831449b9
Filip <filip.kudla@snow.dog>
2020-09-05 23:52:38 +0200

    Rebuild store and delete blogModule with writerModule

--- src/app/model/store.js
+++ src/app/model/store.js
@@ -1,16 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import { blog } from '@/blog/model/blogModule.js'
-import { writer } from '@/writer/model/writerModule.js'
-import { mainModule } from './mainModule.js'
+import { main } from './mainModule.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    mainModule,
-    writer,
-    blog
+    main
   }
 })
