9f2e111270b4066a81dffb6905dd903247f00e5c
Filip <filip.kudla@snow.dog>
2020-09-05 23:53:11 +0200

    Add to main module which is just main now fetching blog endpoint since writer and blog using it

--- src/app/model/mainModule.js
+++ src/app/model/mainModule.js
@@ -1,11 +1,50 @@
-export const mainModule = {
+import axios from 'axios'
+
+export const main = {
   namespaced: true,
   state: {
-    isFetching: false,
-    isLoading: false
+    posts: null,
+    postCount: 0,
+    pagination: {
+      prev: null,
+      next: null
+    }
   },
   mutations: {
+    addPosts (state, data) {
+      state.posts = data
+    },
+    updateCount (state, count) {
+      state.postCount = count
+    },
+    updatePagination (state, data) {
+      state.pagination = data
+    }
   },
   actions: {
+    async fetchBlogEndpoint ({ commit }, params) {
+      try {
+        const { data } = await axios('http://127.0.0.1:8000/api/post/', {
+          params: {
+            format: 'json',
+            ...params
+          }
+        })
+
+        commit('addPosts', data.results)
+        commit('updateCount', data.count)
+        commit('updatePagination', {
+          prev: data.previous,
+          next: data.next
+        })
+      } catch (error) {
+        commit('addPosts', null)
+        commit('updateCount', 0)
+        commit('updatePagination', {
+          prev: null,
+          next: null
+        })
+      }
+    }
   }
 }
